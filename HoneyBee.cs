9ab77f4dcda984c6403fccb6b6590d22a50f9ad3
dmtdenisa <dumitrumaria10@gmail.com>
2022-04-27 00:37:38 +0300

    Add project files.

---
+++ HoneyBee.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace beeAppLibrary
+{
+    public abstract class HoneyBee : Bee
+    {
+        private string _ID;
+
+        public string ID { get => _ID; set => _ID = value; }
+        
+
+        public string showAllInfo()
+        {
+            return ID;
+        }
+
+        public string showRole()
+        {
+            return GetType().Name;
+        }
+    }
+}
