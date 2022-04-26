5dd33ec5dc25fef128e6327f90b9ed2d7425b361
dmtdenisa <dumitrumaria10@gmail.com>
2022-04-27 01:00:22 +0300

    created the hive object and added one static field and a static constructor

---
+++ Hive.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace beeAppLibrary
+{
+    public class Hive
+    {
+        public static DateTime _dateOfCreation;
+
+        static Hive()
+        {
+            _dateOfCreation = DateTime.Now;
+        }
+    }
+}
